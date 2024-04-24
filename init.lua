cf3d2070c9e518a761a53771965f27d66b91d4b8
Andy <andrew.molinaro916@gmail.com>
2024-04-24 18:59:54 -0500

    Added Treesitter, and telescope plugins

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
-
+vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -19,13 +18,31 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-  { "rose-pine/neovim", name = "rose-pine" }
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 
 }
 
 local opts = {}
-
 require("lazy").setup(plugins,opts)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
 require("rose-pine").setup()
 vim.cmd.colorscheme "rose-pine" 
+
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
